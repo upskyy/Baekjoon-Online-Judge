51415cbc7f98f381aa49a4d7e4a2952ce28d0126
HaSangChun <54731898+hasangchun@users.noreply.github.com>
2020-12-26 16:43:31 +0900

    add my study

---
+++ Data-Structures/(9093)단어 뒤집기.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+num = int(input())
+for _ in range(num):
+    string = input()
+    sentence = list()
+    stack1 = list()
+    stack2 = list()
+    for j in string:
+        sentence.append(j)
+    sentence.append('\n')
+    for ch in sentence:
+        if (ch == ' ') or (ch == '\n'):
+            while len(stack1) != 0:
+                top = stack1.pop()
+                stack2.append(top)
+            if ch != '\n':
+                stack2.append(' ')
+        else:
+            stack1.append(ch)
+    print("".join(stack2))
