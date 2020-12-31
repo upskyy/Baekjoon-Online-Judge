31c540133a0753d1501aa8aa7b9b69b6144977e3
HaSangChun <54731898+hasangchun@users.noreply.github.com>
2021-01-01 01:58:11 +0900

    add my study

---
+++ Dynamic-Programming/(11053)가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+num = int(input())
+sequence = list(map(int, input().split()))
+ans = [1 for _ in range(num)]
+for i in range(num):
+    for j in range(i):
+        if (sequence[j] < sequence[i]) and (ans[i] < ans[j] + 1):
+            ans[i] = ans[j] + 1
+print(max(ans))
