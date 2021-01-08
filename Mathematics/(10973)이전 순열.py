8b693a5bfccedf62090fd1248f8391e5a48bd5af
HaSangChun <54731898+hasangchun@users.noreply.github.com>
2021-01-08 18:34:51 +0900

    add my study

---
+++ Mathematics/(10973)이전 순열.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+_ = int(input())
+nums = list(map(int, input().split()))
+
+
+def last_permutation(numbers):
+    i = len(numbers) - 1
+    while i > 0 and numbers[i] > numbers[i - 1]:
+        i -= 1
+    if i == 0:
+        return False
+    j = len(numbers) - 1
+    while numbers[i - 1] < numbers[j]:
+        j -= 1
+
+    numbers[i - 1], numbers[j] = numbers[j], numbers[i - 1]
+    j = len(numbers) - 1
+    while i < j:
+        numbers[i], numbers[j] = numbers[j], numbers[i]
+        i += 1
+        j -= 1
+    return True
+
+
+if last_permutation(nums):
+    print(" ".join(map(str, nums)))
+else:
+    print(-1)
