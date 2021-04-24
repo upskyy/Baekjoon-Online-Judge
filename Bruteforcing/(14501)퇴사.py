3f7f73c85bf5a0b9e066c059d4f63a656aea0831
HaSangChun <54731898+hasangchun@users.noreply.github.com>
2021-04-24 19:04:30 +0900

    add my study

---
+++ Bruteforcing/(14501)퇴사.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
+
+
+def dfs(idx, s):
+    global answer
+    if idx >= n:
+        if answer < s:
+            answer = s
+        return
+
+    if idx + arr[idx][0] <= n:
+        dfs(idx + arr[idx][0], s + arr[idx][1])
+    dfs(idx + 1, s)
+
+
+dfs(0, 0)
+print(answer)
